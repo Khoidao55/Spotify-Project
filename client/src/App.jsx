a18353607934be0632581e6ca64955db2b4b3152
Khoi Dao <khoidao55@gmail.com>
2021-05-05 01:19:42 -0700

    making lofi

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,13 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import './App.css';
 
 const App = () => {
+  useEffect(() => {
+    axios.get('/categories')
+    .then(data => console.log('data:', data))
+    .catch(err => console.log(err))
+  });
   return (
     <div>
       <h1>Khoi's World</h1>
--- server/server.js
+++ server/server.js
@@ -1,13 +1,29 @@
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
+const axios = require('axios');
+const config = require('../config.js');
 const PORT = 3000;
 
 const app = express();
+const url = 'https://api.spotify.com/v1';
+const token = {
+  'User-Agent': 'request',
+  Authorization: config.SPOTIFY_API_KEY,
+};
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(express.static('public'));
+
+app.get('/categories', (req, res) => {
+  axios.get(`${url}/browse/categories`, {
+    headers:token,
+  })
+  .then((data) => res.status(200).send(data.data))
+  .catch((err) => res.status(400).send(err));
+})
 
 app.listen(PORT, () => {
   console.log('Listening on 3000');
