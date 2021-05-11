4c7fe8e64999e1ceb44572b591516fc949e945ab
Khoi Dao <khoidao55@gmail.com>
2021-05-10 20:35:48 -0700

    refreshed the app, will implement spotify Auth

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,7 +5,7 @@ import './App.css';
 const App = () => {
   return (
     <div>
-      <iframe title="This Week’s Playlist" src="https://open.spotify.com/embed/playlist/3LFIBdP7eZXJKqf3guepZ1?si=91df77917f764fb1" width="100%" height="80" class="spotifyPlayer" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
+      <h1>Hello World</h1>
     </div>
   )
 }
